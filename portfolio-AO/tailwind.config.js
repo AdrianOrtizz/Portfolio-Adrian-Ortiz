f247a350de2c0a375641026b9b2a968a8a9154e0
QuemeraT <adrianortizzt1vl@gmail.com>
2024-05-17 09:15:20 -0300

    Se agrega la paleta de colores principal a la configuración de Tailwind

--- portfolio-AO/tailwind.config.js
+++ portfolio-AO/tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mipal: {
+          dark: "#070F2B",
+          nodark: "#1B1A55",
+          gray: "#535C91",
+          DEFAULT: "#9290C3",
+        },
+      },
+    },
+    fontFamily: {
+      oswa: ["Oswald"],
+    },
   },
   plugins: [],
 };
